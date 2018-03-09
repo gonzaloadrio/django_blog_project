0eac0fdefa9c1fcdf6ab5aa4831b987a9d9756cf
Gonzalo Adrio <gadrio@ictel.com>
2018-03-09 13:32:10 +0100

    Fix link to edit

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd^cl9mlx8n!8!23ohkl*r=as($yeed3*&7djsz$re2!$j!w*$-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','0.0.0.0']
 
 
 # Application definition
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
